4aba235fc524be0be970f271ca705e0d16a94572
Laksh Bhambhani <lakshbh@gmail.com>
2020-01-04 07:06:38 -0800

    Fixed setup, added RobotMap

--- 2020-Robot/src/main/java/frc/team670/robot/constants/RobotConstants.java
+++ 2020-Robot/src/main/java/frc/team670/robot/constants/RobotConstants.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.team670.robot;
+package frc.team670.robot.constants;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -15,5 +15,5 @@ package frc.team670.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {
+public final class RobotConstants {
 }
