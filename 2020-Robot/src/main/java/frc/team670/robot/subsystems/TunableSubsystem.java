07a511f7f80ff921edfce6f8ca6e4d453e2228d2
ctchen <claire.t.chen.ca@gmail.com>
2020-01-04 17:45:04 -0800

    r o t a t i o n

---
+++ 2020-Robot/src/main/java/frc/team670/robot/subsystems/TunableSubsystem.java
@@ -0,0 +1,20 @@
+package frc.team670.robot.subsystems;
+
+/**
+ * Base for any tunable subsystem
+ */
+public interface TunableSubsystem {
+
+    public boolean getTimeout();
+
+    /**
+     * Enables percent output to shut off other movement of the Subsystem (note this means it will stop holding itself up).
+     */
+    public void stop();
+
+    /**
+     * Moves the Subsystem using percent outpu
+     */
+    public void moveByPercentOutput(double output);
+
+}
---
+++ 2020-Robot/src/main/java/frc/team670/robot/utils/SparkMAXSetup.java
@@ -0,0 +1,18 @@
+package frc.team670.robot.utils;
+
+public final class SparkMAXSetup{
+
+    public static void factory(int id){
+
+    }
+
+    public static void setConstants(int id, double ramp, double kp, double ki, double kd){
+
+    }
+
+    public static void setFollowerConstants(int id, int masterID){
+
+    }
+
+    
+}
