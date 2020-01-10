81e4cb6caa816bf377c4216b70159377c8c3c725
EffectuatorX <57155887+EffectuatorX@users.noreply.github.com>
2020-01-09 21:06:52 -0800

    initial code for subystem
    created new class for this subsystem, wrote initial code
    
    There are still methods I need figure out how to call correctly.

---
+++ 2020-Robot/src/main/java/frc/team670/robot/subsystems/ColorWheelSpinner.java
@@ -0,0 +1,47 @@
+package frc.team670.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+public class ColorWheelSpinner extends SubsystemBase {
+
+private String referenceColor = "";
+private boolean sawColor;
+public int sawColorCount = 0;
+private double motorSpeed = 0.8;
+private boolean rotationControlTriggered = false;
+private VictorSPX victor;
+
+public ColorWheelSpinner() {
+    victor = new VictorSPX(1); //TODO: Constructor takes in CAN ID
+}
+
+public void whenButtonPressed() {
+	rotationControlTriggered = true;
+	colorSeen = referenceColor;
+	victor.set(ControlMode.PercentOutput, motorSpeed);
+}
+
+
+public void periodic() {
+
+if (rotationControlTriggered) {
+    if (colorSeen = referenceColor) {
+        sawColor = true;
+    }
+
+    if (sawColor && colorSeen != referenceColor) {
+        sawColorCount ++;
+        sawColor = false;
+    }
+
+    if (sawColorCount == 9) {
+        victor.stopMotor();
+    }
+}
+    
+}
+
+}
