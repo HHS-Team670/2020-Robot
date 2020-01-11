cec3357024169d7a9c8dc6b4daee0bdae0aa7586
Lily Collins <lilyccollins369@gmail.com>
2020-01-11 11:53:18 -0800

    method to rotate the wheel a given number of times and method to move wheel to a given color

--- 2020-Robot/src/main/java/frc/team670/robot/subsystems/ColorWheelSpinner.java
+++ 2020-Robot/src/main/java/frc/team670/robot/subsystems/ColorWheelSpinner.java
@@ -1,5 +1,6 @@
 package frc.team670.robot.subsystems;
 
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.team670.robot.RobotContainer;
 import frc.team670.robot.constants.RobotConstants;
@@ -18,6 +19,30 @@ public class ColorWheelSpinner extends SubsystemBase {
     public String detectColor() {
         return RobotContainer.colorMatch.detectColor();
     }
+
+    public void moveToColor(String color) {
+        setSpeed(0.5);
+        while (!detectColor().equals(color)) {}
+        setSpeed(0);
+    }
+
+    public void rotate(int rotations) {
+        String c = detectColor();
+        int i = 1;
+        setSpeed(0.5);
+        while (i < rotations*2)  {
+            if (detectColor().equals(c))
+                i++;
+        }
+        setSpeed(0);
+    }
+
+    // public void rotateSection(int sections) { //comment out for testing
+    //     String c = detectColor();
+    //     int i = 0;
+    //     while(detectColor().equals(c)) {
+    //     }
+    // }
 
     public ColorWheelSpinner() {
         victor = new VictorSPX(RobotConstants.COLOR_WHEEL_MOTOR_CAN_ID); //TODO: Constructor takes in CAN ID
