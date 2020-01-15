795b39d12e2a953e155423f5aac91358e767c504
ctchen <claire.t.chen.ca@gmail.com>
2020-01-15 15:29:41 -0800

    I am slow

--- 2020-Robot/src/main/java/frc/team670/robot/subsystems/MustangSubsystem.java
+++ 2020-Robot/src/main/java/frc/team670/robot/subsystems/MustangSubsystem.java
@@ -4,20 +4,13 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SelectCommand;
-import frc.team670.robot.commands.MustangCommand;
+import frc.team670.robot.commands.MustangCommandBase;
 import frc.team670.robot.RobotContainer;
 
-import java.util.Map;
-import static java.util.Map.entry;
-import java.util.ArrayList;
 
 public abstract class MustangSubsystem extends SubsystemBase{
 
     private HealthState lastHealthState;
-
-    private ArrayList<MustangCommand> greenCommands;
-    private ArrayList<MustangCommand> yellowCommands;
-    private ArrayList<MustangCommand> redCommands;
 
     public MustangSubsystem(){
         RobotContainer.addSubsystem(this);
@@ -38,16 +31,9 @@ public abstract class MustangSubsystem extends SubsystemBase{
         lastHealthState = checkHealth();
     }
 
-    public void initDefaultCommand(MustangCommand command){
+    public void initDefaultCommand(MustangCommandBase command){
         CommandScheduler.getInstance().setDefaultCommand(this, command);
     }
-
-    public void addCommandForState(HealthState state, MustangCommand command){
-        if (state == HealthState.GREEN) greenCommands.add(command);
-        if (state == HealthState.YELLOW) yellowCommands.add(command);
-        if (state == HealthState.RED) redCommands.add(command);
-    }
-
   
 
 }
