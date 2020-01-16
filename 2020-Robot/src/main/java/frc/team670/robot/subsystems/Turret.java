079f9f0172ea886e83cc4d95d583100aea842214
Kenny Wu <kennys.dope@gmail.com>
2020-01-15 17:53:54 -0800

    addedIsworking
    added encoder functionality method.
    
    May not function properly, tentative to change

--- 2020-Robot/src/main/java/frc/team670/robot/subsystems/Turret.java
+++ 2020-Robot/src/main/java/frc/team670/robot/subsystems/Turret.java
@@ -7,37 +7,77 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.SensorCollection;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.EncoderType;
+import com.revrobotics.CANAnalog.AnalogMode;
 
+import edu.wpi.first.wpilibj2.command.ScheduleCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.team670.robot.RobotContainer;
 
 public class Turret extends RotatingSubsystem
 {
-    private CANEncoder encoder;
+    private CANSparkMax speedController;
+    private int countsPerRevolution;
 
-    public Turret(TalonSRX rotator, int mainCANId, CANEncoder encoder)
+    public Turret(TalonSRX rotator, int mainCANId, CANSparkMax motor, int cpm)
     {
         super(rotator, 0, 180, 0, true, 0, 0, 0, 0, 0); //TODO Complete this
-        this.encoder = encoder;
+        this.speedController = motor;
+        this.countsPerRevolution = cpm;
     }
 
     
     public double getAngleInDegrees()
     {
         //there are 4096 ticks in a circle, so one degree is 11 17/45 ticks.
-        return super.getPositionTicks() / (11.0 + (17.0/45.0));
+        return super.getPositionTicks() / (getTicksInDegrees());
     }
 
     /**
-     * TO DO: implement this method
+     * TODO: implement this method
      */
     protected double getArbitraryFeedForwardAngleMultiplier()
     {
         return 0.0;
     }
 
+    /**
+     * TODO: Maybe change it
+     */
+    public double getSpeed() {
+        return speedController.get();
+    }
+
+
+    public boolean isWorking(double previousTicks) {
+        double numberOfTicks = getEncoder().getPosition();
+
+        // calculate potential travel in ticks
+
+        double power = getSpeed();
+        return power != 0 && numberOfTicks != previousTicks;
+        
+
+        /**
+         * get encoder ticks
+         * 
+         * get power of motor, 
+         *      calculate potential ticks travelled. 
+         * 
+         * if encoder - potential = minorDifference. 
+         *      we are good
+         * else
+         *      red flag
+         * 
+         */
+        
+    }
+
+
     public double getTicksInDegrees()
     {
-        return getEncoder().getCountsPerRevolution();
+        return getEncoder().getCountsPerRevolution()/360.0;
     }
 
      /**
@@ -45,12 +85,12 @@ public class Turret extends RotatingSubsystem
      */
     public void setMotionMagicSetpointAngle(double angle)
     {
-        setpoint = (int)(angle*(11.0 + 17.0/45.0));
+        setpoint = (int)(angle*(getTicksInDegrees()));
     }
 
     public CANEncoder getEncoder()
     {
-        return encoder;
+        return speedController.getEncoder(EncoderType.kHallSensor, countsPerRevolution);
     }
 
 }
