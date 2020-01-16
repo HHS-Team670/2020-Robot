b56d4940816192d0dc595b8292493bcba95e6c75
Eric Chang <ericschang05@gmail.com>
2020-01-15 16:26:44 -0800

    Changed motor controller from TalonSRX to SPARK MAX

--- 2020-Robot/src/main/java/frc/team670/robot/subsystems/Intake.java
+++ 2020-Robot/src/main/java/frc/team670/robot/subsystems/Intake.java
@@ -2,23 +2,28 @@ package frc.team670.robot.subsystems;
 
 import frc.team670.robot.constants.RobotMap;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+// import com.ctre.phoenix.motorcontrol.ControlMode;
+// import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+
 public class Intake extends SubsystemBase {
     private Compressor comp;
     private Solenoid deployer;
-    private TalonSRX roller;
+    // private TalonSRX roller;
+    private CANSparkMax roller;
     private boolean isDeployed;
 
     public Intake() {
         comp = new Compressor(RobotMap.PCMODULE);
         comp.setClosedLoopControl(true);
-        roller = new TalonSRX(RobotMap.INTAKE_ROLLER);
+        // roller = new TalonSRX(RobotMap.INTAKE_ROLLER);
+        roller = new CANSparkMax(RobotMap.INTAKE_ROLLER, CANSparkMaxLowLevel.MotorType.kBrushless);
         deployer = new Solenoid(RobotMap.PCMODULE, RobotMap.INTAKE_DEPLOYER);
     }
 
@@ -28,7 +33,8 @@ public class Intake extends SubsystemBase {
     }
     
     public void rollIntake(double speed) {
-        roller.set(ControlMode.PercentOutput, speed);
+        // roller.set(ControlMode.PercentOutput, speed);
+        roller.set(speed);
     }
 
     public boolean isDeployed() {
