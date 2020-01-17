a2e2ab8e233edf4d2cd469a29e0de520067e9bd1
kale <kalebkim1939@gmail.com>
2020-01-16 19:43:27 -0800

    Update RobotMap.java
    also import code from lily branch

--- 2020-Robot/src/main/java/frc/team670/robot/constants/RobotMap.java
+++ 2020-Robot/src/main/java/frc/team670/robot/constants/RobotMap.java
@@ -4,9 +4,11 @@ import edu.wpi.first.wpilibj.SerialPort.Port;
 import edu.wpi.first.wpilibj.SerialPort;
 
 public class RobotMap{
-    public static final int PDP_ID = 0;
+  
+  public static final int PDP_ID = 0;
 
-    public static final int DRIVER_CONTROLLER_PORT=0;
+  public static final int DRIVER_CONTROLLER_PORT=0;
+  public static final int OPERATOR_CONTROLLER_PORT=1;
 
   // Drive Base
   public static final int SPARK_LEFT_MOTOR_1 = 20; // These are properly set. 
@@ -14,13 +16,15 @@ public class RobotMap{
   public static final int SPARK_RIGHT_MOTOR_1 = 22;
   public static final int SPARK_RIGHT_MOTOR_2 = 23;
 
-  //Encoders
-      //Drivebase
-  public static final int LEFT_ENCODER_CHANNEL_A = 0; // These are properly set
-  public static final int LEFT_ENCODER_CHANNEL_B = 1;
-  public static final int RIGHT_ENCODER_CHANNEL_A = 2;
-  public static final int RIGHT_ENCODER_CHANNEL_B = 3;
-
+  // NavX
   public final static Port NAVX_PORT = SerialPort.Port.kUSB;  
+
+  //PCModule
+  public final static int PCMODULE = 2;
+
+  //Intake
+  public final static int INTAKE_SOLENOID = 0;
+  public final static int INTAKE_ROLLERS = 4;
+  public final static int INTAKE_IRSENSOR = 5;
 
 }
