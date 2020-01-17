34ea7f5eede71563b91489ac6cede1e105e1811b
kale <kalebkim1939@gmail.com>
2020-01-16 21:20:07 -0800

    drivebase to robotmap update
    added 4 important variables to drivebase

--- 2020-Robot/src/main/java/frc/team670/robot/constants/RobotMap.java
+++ 2020-Robot/src/main/java/frc/team670/robot/constants/RobotMap.java
@@ -15,6 +15,11 @@ public class RobotMap {
     public static final int SPARK_RIGHT_MOTOR_1 = 22;
     public static final int SPARK_RIGHT_MOTOR_2 = 23;
 
+    public static final int LEFT_ENCODER_CHANNEL_A = 0; // These are properly set
+    public static final int LEFT_ENCODER_CHANNEL_B = 1;
+    public static final int RIGHT_ENCODER_CHANNEL_A = 2;
+    public static final int RIGHT_ENCODER_CHANNEL_B = 3;
+
     // NavX
     public final static Port NAVX_PORT = SerialPort.Port.kUSB;  
 
