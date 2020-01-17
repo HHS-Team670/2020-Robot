2be41a7e160789bfc2a5d5e138566a5f8f5de204
kale <kalebkim1939@gmail.com>
2020-01-16 20:01:54 -0800

    cleaned cohdeh

--- 2020-Robot/src/main/java/frc/team670/robot/constants/RobotMap.java
+++ 2020-Robot/src/main/java/frc/team670/robot/constants/RobotMap.java
@@ -3,28 +3,26 @@ package frc.team670.robot.constants;
 import edu.wpi.first.wpilibj.SerialPort.Port;
 import edu.wpi.first.wpilibj.SerialPort;
 
-public class RobotMap{
-  
-  public static final int PDP_ID = 0;
+public class RobotMap {
+    public static final int PDP_ID = 0;
 
-  public static final int DRIVER_CONTROLLER_PORT=0;
-  public static final int OPERATOR_CONTROLLER_PORT=1;
+    public static final int DRIVER_CONTROLLER_PORT = 0;
+    public static final int OPERATOR_CONTROLLER_PORT = 1;
 
-  // Drive Base
-  public static final int SPARK_LEFT_MOTOR_1 = 20; // These are properly set. 
-  public static final int SPARK_LEFT_MOTOR_2 = 21; 
-  public static final int SPARK_RIGHT_MOTOR_1 = 22;
-  public static final int SPARK_RIGHT_MOTOR_2 = 23;
+    // Drive Base CAN ids
+    public static final int SPARK_LEFT_MOTOR_1 = 20; // These are properly set. 
+    public static final int SPARK_LEFT_MOTOR_2 = 21; 
+    public static final int SPARK_RIGHT_MOTOR_1 = 22;
+    public static final int SPARK_RIGHT_MOTOR_2 = 23;
 
-  // NavX
-  public final static Port NAVX_PORT = SerialPort.Port.kUSB;  
+    // NavX
+    public final static Port NAVX_PORT = SerialPort.Port.kUSB;  
 
-  //PCModule
-  public final static int PCMODULE = 2;
+    // PCModule
+    public final static int PCMODULE = 2;
 
-  //Intake
-  public final static int INTAKE_SOLENOID = 0;
-  public final static int INTAKE_ROLLERS = 4;
-  public final static int INTAKE_IRSENSOR = 5;
-
+    // Intake
+    public final static int INTAKE_SOLENOID = 0;
+    public final static int INTAKE_ROLLERS = 4;
+    public final static int INTAKE_IRSENSOR = 5;
 }
