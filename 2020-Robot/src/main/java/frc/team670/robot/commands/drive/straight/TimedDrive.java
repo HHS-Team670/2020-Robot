3d2eae9872a33228f5e2e9da3e1ed6dd0aaaf2b0
ctchen <claire.t.chen.ca@gmail.com>
2020-01-18 23:35:20 -0800

    Any problems from merging should be gone now

--- 2020-Robot/src/main/java/frc/team670/robot/commands/drive/straight/TimedDrive.java
+++ 2020-Robot/src/main/java/frc/team670/robot/commands/drive/straight/TimedDrive.java
@@ -8,19 +8,17 @@
 package frc.team670.robot.commands.drive.straight;
 
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.team670.robot.subsystems.DriveBase;
+import frc.team670.robot.RobotContainer;
 import frc.team670.robot.utils.Logger;
 
 
 
 public class TimedDrive extends WaitCommand {
 
-  private DriveBase driveBase;
 
-  public TimedDrive(DriveBase driveBase, int seconds) {
+  public TimedDrive(int seconds) {
     super(seconds);
-    this.driveBase = driveBase;
-    addRequirements(this.driveBase);
+    addRequirements(RobotContainer.driveBase);
   }
 
   // Called repeatedly when this Command is scheduled to run
@@ -33,8 +31,8 @@ public class TimedDrive extends WaitCommand {
   // Called once after isFinished returns true
   @Override
   public void end(boolean isInteruppted) {
-    driveBase.stop();
-    Logger.consoleLog("Drivebase stopped");
+    RobotContainer.driveBase.stop();
+    Logger.consoleLog("Robot stopped");
   }
 
 }
