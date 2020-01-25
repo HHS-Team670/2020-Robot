5021b65c020718df0d611d212a1ccb903d2c0ece
Kenny Wu <kennys.dope@gmail.com>
2020-01-24 18:09:50 -0800

    Update RotateTurret.java
    Added fields and constructor to RotateTurret

--- 2020-Robot/src/main/java/frc/team670/robot/commands/turret/RotateTurret.java
+++ 2020-Robot/src/main/java/frc/team670/robot/commands/turret/RotateTurret.java
@@ -1,8 +1,15 @@
 package frc.team670.robot.commands.turret;
+import frc.team670.robot.subsystems.Turret;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class RotateTurret extends CommandBase{
+    private Turret turret;
+    public RotateTurret(Turret turret){
 
+       this.turret = turret;
+
+    }
+    
         
 }
