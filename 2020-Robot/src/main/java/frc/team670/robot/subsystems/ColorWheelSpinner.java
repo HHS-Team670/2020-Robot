3ff98a133e24a31624bb95ff219aa66b354e645e
ctchen <claire.t.chen.ca@gmail.com>
2020-01-29 22:20:55 -0800

    Update ColorWheelSpinner.java

--- 2020-Robot/src/main/java/frc/team670/robot/subsystems/ColorWheelSpinner.java
+++ 2020-Robot/src/main/java/frc/team670/robot/subsystems/ColorWheelSpinner.java
@@ -1,30 +1,34 @@
 package frc.team670.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.team670.robot.constants.RobotMap;
 import frc.team670.robot.dataCollection.sensors.ColorMatcher;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-public class ColorWheelSpinner extends SubsystemBase {
+public class ColorWheelSpinner extends MustangSubsystemBase {
 
     private static ColorMatcher colorMatch;
     private CANSparkMax rotator;
 
-    public final double MOTOR_SPEED = 1.0; // TODO: tune for actual motor
+    public final double MOTOR_SPEED = 0.1; // TODO: tune for actual motor
 
     public ColorWheelSpinner() {
         colorMatch = new ColorMatcher();
-        colorMatch.init();
-        rotator = new CANSparkMax(RobotMap.COLOR_WHEEL_MOTOR_CAN_ID, MotorType.kBrushless); // Constructor takes in CAN ID
+        rotator = new CANSparkMax(RobotMap.COLOR_WHEEL_MOTOR_ID, MotorType.kBrushless); // Constructor takes in CAN ID
     }
-    
+
     public void setSpeed(double motorSpeed) {
         rotator.set(motorSpeed);
     }
 
     public int detectColor() {
         return colorMatch.detectColor();
-    }    
+    }
+
+    @Override
+    public HealthState checkHealth() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
