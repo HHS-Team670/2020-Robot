7fde24fc9a1ba5328a1c5844203ae28a440550bf
ctchen <claire.t.chen.ca@gmail.com>
2020-02-07 17:48:43 -0800

    Implemented suggestions, ready for testing

--- 2020-Robot/src/main/java/frc/team670/robot/subsystems/Intake.java
+++ 2020-Robot/src/main/java/frc/team670/robot/subsystems/Intake.java
@@ -1,60 +1,47 @@
 package frc.team670.robot.subsystems;
 
 import frc.team670.robot.constants.RobotMap;
-import frc.team670.robot.dataCollection.sensors.IRSensor;
 import frc.team670.robot.utils.motorcontroller.MotorConfig.Motor_Type;
-import frc.team670.robot.utils.motorcontroller.MotorConfig;
+import frc.team670.robot.utils.motorcontroller.SparkMAXLite;
 
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-import com.revrobotics.CANEncoder;
-import com.revrobotics.CANPIDController;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.ControlType;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import frc.team670.robot.utils.motorcontroller.SparkMAXFactory;
 
 /*
  * @author Khicken
 */
 public class Intake extends MustangSubsystemBase {
 
-    private CANSparkMax roller;
+    private SparkMAXLite roller;
     private Compressor compressor;
     private Solenoid deployer;
-    private int currentLimit;
     private boolean isDeployed;
-    
-    public Intake() {
 
-        roller = new CANSparkMax(RobotMap.INTAKE_ROLLER, MotorType.kBrushless);
+    public Intake() {
+        roller = SparkMAXFactory.buildFactorySparkMAX(RobotMap.INTAKE_ROLLER, Motor_Type.NEO_550);
         compressor = new Compressor(RobotMap.INTAKE_COMPRESSOR);
         compressor.setClosedLoopControl(true);
-		deployer = new Solenoid(RobotMap.PCMODULE, RobotMap.INTAKE_DEPLOYER);
-
-        currentLimit = 20;
-        roller.setSmartCurrentLimit(currentLimit);
-
+        deployer = new Solenoid(RobotMap.PCMODULE, RobotMap.INTAKE_DEPLOYER);
     }
+
     public boolean isRolling() {
         return roller.get() != 0;
     }
 
-	public void deploy(boolean isDeployed)
-	{
-        deployer.set(isDeployed);
-        isDeployed = true;
+    public void deploy(boolean isDeployed) {
+        this.isDeployed = isDeployed;
     }
-    
+
     public boolean isDeployed() {
         return isDeployed;
     }
 
     public void roll(double speed) {
-        roller.set(speed);
+        if (isDeployed) {
+            roller.set(speed);
+        }
     }
-
 
     @Override
     public HealthState checkHealth() {
