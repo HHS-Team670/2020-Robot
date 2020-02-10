e685bc30b0031e41a4586fadbff225def2eaa44b
ctychen <34198336+ctychen@users.noreply.github.com>
2020-02-09 23:49:59 -0800

    MustangCoprocessor, setting up for vision

--- 2020-Robot/src/main/java/frc/team670/robot/constants/RobotMap.java
+++ 2020-Robot/src/main/java/frc/team670/robot/constants/RobotMap.java
@@ -10,12 +10,15 @@ public class RobotMap {
   public static final int DRIVER_CONTROLLER_PORT = 0;
   public static final int OPERATOR_CONTROLLER_PORT = 1;
 
+  /** Port on the PCM for the vision LEDs (the extremely bright ones) */
+  public static final int VISION_LED_PCM = 4;
+
   // Drive Base
   public static final int SPARK_LEFT_MOTOR_1 = 20; // These are properly set.
   public static final int SPARK_LEFT_MOTOR_2 = 21;
   public static final int SPARK_RIGHT_MOTOR_1 = 22;
   public static final int SPARK_RIGHT_MOTOR_2 = 23;
-  
+
   // NavX
   public final static Port NAVX_PORT = SerialPort.Port.kUSB;
 
@@ -23,11 +26,11 @@ public class RobotMap {
   public final static int SHOOTER_STAGE_2_MAIN = 11;
   public final static int SHOOTER_STAGE_2_FOLLOWER = 12;
   public static final int SHOOTER_STAGE_1 = 13;
-  
-   // PC Module
-   public static final int PCMODULE = 2;
 
-  //Turret 
+  // PC Module
+  public static final int PCMODULE = 2;
+
+  // Turret
   public static final int TURRET_ROTATOR = 24;
 
   // Motors
@@ -35,16 +38,16 @@ public class RobotMap {
 
   // Intake
   public static final int INTAKE_ROLLER = 4;
-  public static final int INTAKE_DEPLOYER = 3;    //SET THIS
-  public static final int INTAKE_COMPRESSOR = 6;  //SET THIS
+  public static final int INTAKE_DEPLOYER = 3; // SET THIS
+  public static final int INTAKE_COMPRESSOR = 6; // SET THIS
 
   // Conveyor
   public static final int CONVEYOR_ROLLER = 8;
 
-  //Indexer and Updraw
+  // Indexer and Updraw
   public static final int INDEXER_ROTATOR = 9;
   public static final int UPDRAW_SPINNER = 10;
 
-public static final int SHOOTER_2_MAIN = 0;
+  public static final int SHOOTER_2_MAIN = 0;
 
 }
