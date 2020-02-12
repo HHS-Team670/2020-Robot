17933d082de5e903e9a11de1517edc82cd04b0ca
sviolet767 <55167235+sviolet767@users.noreply.github.com>
2020-02-11 18:46:55 -0800

    Added Climber commands

---
+++ 2020-Robot/src/main/java/frc/team670/robot/commands/climber/ExtendClimber.java
@@ -0,0 +1,47 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+ 
+package frc.team670.robot.commands.climber;
+import frc.team670.robot.subsystems.climber.Climber;
+ 
+import edu.wpi.first.wpilibj2.command.CommandBase;
+ 
+public class ExtendClimber extends CommandBase {
+  private Climber climber;
+  private double heightCM;
+ 
+  public ExtendClimber(Climber climber, double heightCM) {
+    super();
+    this.climber = climber;
+    this.heightCM = heightCM;
+    addRequirements(climber);
+  }
+ 
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    climber.set(0);
+  }
+ 
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    climber.climb(heightCM);
+  }
+ 
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    climber.set(0); //TODO: check to make sure this is right
+  }
+ 
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return climber.isAtTarget();
+  }
+}
