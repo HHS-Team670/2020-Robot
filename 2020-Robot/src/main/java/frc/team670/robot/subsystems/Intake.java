f9fe3fb11ce10a1c082f0a7493a8a4b5b9892a12
ctychen <34198336+ctychen@users.noreply.github.com>
2020-02-17 18:50:51 -0800

    Intake unjam works for now

--- 2020-Robot/src/main/java/frc/team670/robot/subsystems/Intake.java
+++ 2020-Robot/src/main/java/frc/team670/robot/subsystems/Intake.java
@@ -3,6 +3,9 @@ package frc.team670.robot.subsystems;
 import frc.team670.robot.constants.RobotMap;
 import frc.team670.robot.utils.motorcontroller.MotorConfig.Motor_Type;
 import frc.team670.robot.utils.motorcontroller.SparkMAXLite;
+
+import frc.team670.robot.utils.Logger;
+
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -16,15 +19,17 @@ public class Intake extends MustangSubsystemBase {
     private SparkMAXLite roller;
     private Compressor compressor;
     private Solenoid deployer;
-    private boolean isDeployed;
+    private boolean isDeployed = true; //TODO: true for testing, change this
 
     private double INTAKE_ROLLER_SPEED = 0.6; // From testing 2/16
+    private double INTAKE_PEAK_CURRENT = 19; // TODO: figure this out
+    private int exceededCurrentLimitCount = 0;
 
     public Intake() {
         // Intake roller should be inverted
         roller = SparkMAXFactory.buildFactorySparkMAX(RobotMap.INTAKE_ROLLER, Motor_Type.NEO_550);
         roller.setInverted(true);
-        
+        roller.setOpenLoopRampRate(1.0);
         compressor = new Compressor(RobotMap.PCMODULE);
         compressor.setClosedLoopControl(true);
         SmartDashboard.putNumber("Intake Speed", 0.0);
@@ -54,6 +59,23 @@ public class Intake extends MustangSubsystemBase {
         }
     }
 
+    public boolean isJammed(){
+        double intakeCurrent = roller.getOutputCurrent();
+        Logger.consoleLog("Intake current: %s", intakeCurrent);
+        if (intakeCurrent > 0.2){
+            if (intakeCurrent >= INTAKE_PEAK_CURRENT) {
+                exceededCurrentLimitCount++;
+            } else {
+                exceededCurrentLimitCount = 0;
+            }
+            if (exceededCurrentLimitCount >= 4){ // 4 consecutive readings higher than peak
+                return true;
+            }
+        }
+        return false;
+
+    }
+
     public void stop() {
         roller.set(0);
     }
@@ -81,6 +103,7 @@ public class Intake extends MustangSubsystemBase {
 
     @Override
     public void mustangPeriodic() {
+
     }
 
     public void test() {
