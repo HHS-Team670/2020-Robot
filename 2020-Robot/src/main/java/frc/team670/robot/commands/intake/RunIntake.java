fcf3e83fe063760fa43b2c83f00fb23aea190056
ctychen <34198336+ctychen@users.noreply.github.com>
2020-02-17 18:52:21 -0800

    RunIntake tested

--- 2020-Robot/src/main/java/frc/team670/robot/commands/intake/RunIntake.java
+++ 2020-Robot/src/main/java/frc/team670/robot/commands/intake/RunIntake.java
@@ -8,6 +8,8 @@ import frc.team670.robot.commands.MustangCommand;
 import frc.team670.robot.subsystems.Intake;
 import frc.team670.robot.subsystems.MustangSubsystemBase;
 import frc.team670.robot.subsystems.MustangSubsystemBase.HealthState;
+import frc.team670.robot.utils.Logger;
+
 /**
  *
  */
@@ -16,25 +18,40 @@ public class RunIntake extends CommandBase implements MustangCommand {
 	Map<MustangSubsystemBase, HealthState> healthReqs;
 	private boolean reversed;
 	private Intake intake;
+	private int countWasJammed;
 
 	/**
-	 * @param reversed true to run the intake in reverse (out), false to run it normally (in)
+	 * @param reversed true to run the intake in reverse (out), false to run it
+	 *                 normally (in)
 	 */
-	public RunIntake (boolean reversed, Intake intake) {
+	public RunIntake(boolean reversed, Intake intake) {
 		this.reversed = reversed;
 		this.intake = intake;
 		healthReqs = new HashMap<MustangSubsystemBase, HealthState>();
 		healthReqs.put(intake, HealthState.YELLOW);
 		addRequirements(intake);
+		countWasJammed = 0;
 	}
-	
+
 	public void initialize() {
 		intake.roll(reversed);
 	}
 
+	public void execute() {
+		if (intake.isJammed()) {
+			countWasJammed = 25;
+		}
+		if (countWasJammed > 0) {
+			intake.roll(!reversed);
+			Logger.consoleLog("Intake jammed, running in reverse");
+			countWasJammed--;
+		} else {
+			intake.roll(reversed);
+			Logger.consoleLog("Intake not jammed, running normally");
+		}
+	}
 
 	public Map<MustangSubsystemBase, HealthState> getHealthRequirements() {
 		return healthReqs;
 	}
 }
-
