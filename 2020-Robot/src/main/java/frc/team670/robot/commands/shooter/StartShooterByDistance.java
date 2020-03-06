b2fa5a4d480759d3fbc149c977cd958d54a3cc46
ctychen <34198336+ctychen@users.noreply.github.com>
2020-03-06 06:48:38 -0800

    Update StartShooterByDistance.java

--- 2020-Robot/src/main/java/frc/team670/robot/commands/shooter/StartShooterByDistance.java
+++ 2020-Robot/src/main/java/frc/team670/robot/commands/shooter/StartShooterByDistance.java
@@ -5,7 +5,9 @@ import java.util.Map;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.team670.robot.commands.MustangCommand;
+import frc.team670.robot.constants.FieldConstants;
 import frc.team670.robot.dataCollection.MustangCoprocessor;
+import frc.team670.robot.subsystems.DriveBase;
 import frc.team670.robot.subsystems.MustangSubsystemBase;
 import frc.team670.robot.subsystems.Shooter;
 import frc.team670.robot.subsystems.MustangSubsystemBase.HealthState;
@@ -13,14 +15,14 @@ import frc.team670.robot.subsystems.MustangSubsystemBase.HealthState;
 public class StartShooterByDistance extends CommandBase implements MustangCommand {
 
     private Shooter shooter;
-    private MustangCoprocessor coprocessor;
+    private DriveBase driveBase;
     private double targetRPM;
     private double distanceToTarget;
     private Map<MustangSubsystemBase, HealthState> healthReqs;
 
-    public StartShooterByDistance(Shooter shooter, MustangCoprocessor pi){
+    public StartShooterByDistance(Shooter shooter, DriveBase driveBase){
         this.shooter = shooter;
-        this.coprocessor = pi;
+        this.driveBase = driveBase;
         addRequirements(shooter);
         healthReqs = new HashMap<MustangSubsystemBase, HealthState>();
         healthReqs.put(shooter, HealthState.GREEN);
@@ -28,7 +30,12 @@ public class StartShooterByDistance extends CommandBase implements MustangComman
 
     @Override
     public void initialize() {
-        distanceToTarget = coprocessor.getDistanceToTargetCm();
+        double currentX = driveBase.getPose().getTranslation().getX();
+        double currentY = driveBase.getPose().getTranslation().getY();
+        double distanceToTarget = Math.sqrt(
+            (Math.pow(currentX - FieldConstants.FIELD_ORIGIN_TO_OUTER_GOAL_CENTER_X_METERS, 2) +
+             Math.pow(currentY, 2))
+        );
         targetRPM = shooter.getTargetRPMForDistance(distanceToTarget);
         shooter.setVelocityTarget(targetRPM);
         shooter.setRampRate(true);
