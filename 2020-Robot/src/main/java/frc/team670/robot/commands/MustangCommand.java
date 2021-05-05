763937d577d455dea196f485ad10e4d43a4c195a
CT Chen <claire.t.chen.ca@gmail.com>
2021-05-04 18:51:29 -0700

    put back PID values

--- 2020-Robot/src/main/java/frc/team670/robot/commands/MustangCommand.java
+++ 2020-Robot/src/main/java/frc/team670/robot/commands/MustangCommand.java
@@ -70,6 +70,7 @@ public interface MustangCommand{
                              RobotConstants.kMaxAccelerationMetersPerSecondSquared)
             // Add kinematics to ensure max speed is actually obeyed
             .setKinematics(RobotConstants.kDriveKinematics)
+            .setReversed(true)
             // Apply the voltage constraint
             // .addConstraint(RobotConstants.kAutoPathConstraints)
             .addConstraint(autoVoltageConstraint);
@@ -77,18 +78,18 @@ public interface MustangCommand{
     // An example trajectory to follow.  All units in meters.
     Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
         // Start at the origin facing the +X direction
-        new Pose2d(0, 0, new Rotation2d(0)),
+        new Pose2d(0, 0, new Rotation2d(180)),
         // Pass through these two interior waypoints, making an 's' curve path
         List.of(
             new Translation2d(1, 0),
-            new Translation2d(2, -0),
-            new Translation2d(3, -0),
-            new Translation2d(4, -0),
-            new Translation2d(5, -0)
+            new Translation2d(2, 0)
+            //new Translation2d(3, -0),
+            //n Translation2d(2, -0),
+            //new Translation2d(1, -0)
 
         ),
         // End 6 meters straight ahead of where we started, facing forward
-        new Pose2d(6, 0, new Rotation2d(0)),
+        new Pose2d(3, 0, new Rotation2d(180)),
         // Pass config
         config
     );
@@ -105,7 +106,7 @@ public interface MustangCommand{
     RamseteCommand ramseteCommand = new RamseteCommand(
         exampleTrajectory,
         driveBase::getPose,
-        disabledRamsete,
+        new RamseteController(RobotConstants.kRamseteB, RobotConstants.kRamseteZeta),
         new SimpleMotorFeedforward(//RobotConstants.leftKsVolts,
                                     SmartDashboard.getNumber("leftKs", 0),
                                     SmartDashboard.getNumber("leftKv", 0),
