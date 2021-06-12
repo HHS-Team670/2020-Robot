f243051c2cfee99484b09ce487591b3ad9aeb26e
clearlyconfused <56371094+tarinimaram@users.noreply.github.com>
2021-06-12 11:43:38 -0700

    trying different ending poses for center5diagonal

--- 2020-Robot/src/main/java/frc/team670/paths/twentytwentyone/Center5Diagonal.java
+++ 2020-Robot/src/main/java/frc/team670/paths/twentytwentyone/Center5Diagonal.java
@@ -9,20 +9,29 @@ import frc.team670.robot.subsystems.DriveBase;
 
 /**
  * 2021 field
- * trajectory starting to the left of the port (in perspective of the driver) and going under the switch to pick up 2 balls in a line
+ * trajectory starting from the center of the port (in perspective of the driver) and going diagonally into the switch to pick up as many of 5 balls as possible
  * front of robot starts on initiation line
  * google doc link: https://docs.google.com/document/d/1AsYxFidJCVz2cBFPJEAD1MU-b7dWLGTkcGLwushU1f8/edit?usp=sharing
- * @author megchoy
+ * @author tarini
  */
 public class Center5Diagonal extends Path{
 
         public Center5Diagonal (DriveBase driveBase) {
                 super(
                         List.of(
+                                // wider ending turn
                                 new Pose2d(3.09, -2.628, Rotation2d.fromDegrees(0)),
                                 new Pose2d(5.662, -2.158, Rotation2d.fromDegrees(-5.03)), 
-                                new Pose2d(6.849, -2.604, Rotation2d.fromDegrees(-39.46)),
-                                new Pose2d(6.688, -5.028, Rotation2d.fromDegrees(-129.34))
+                                new Pose2d(6.849, -2.604, Rotation2d.fromDegrees(-39.46)), // experiment between wider or tighter turn
+                                new Pose2d(6.688, -5.028, Rotation2d.fromDegrees(-129.34)) // experiment with ending point (wider or tighter turn)
+
+                                // tighter ending turn
+                                /*
+                                new Pose2d(3.09, -2.628, Rotation2d.fromDegrees(0)),
+                                new Pose2d(5.662, -2.158, Rotation2d.fromDegrees(-5.03)),
+                                new Pose2d(6.874, -2.851, Rotation2d.fromDegrees(-65.28)), // experiment between wider or tighter turn
+                                new Pose2d(6.033, -4.335, Rotation2d.fromDegrees(-159.174)) // experiment with ending point (wider or tighter turn)
+                                */
                         ), 
                 driveBase);
         }
