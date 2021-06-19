2a8a012a0814ff60a8902e63380dcc69043c0e68
Megan Choy <megchoy3@gmail.com>
2021-06-19 11:49:35 -0700

    splitting up TrenchTo3BallLine

--- 2020-Robot/src/main/java/frc/team670/paths/twentytwentyone/TrenchTo3BallLine_GODSPEED2021pt2.java
+++ 2020-Robot/src/main/java/frc/team670/paths/twentytwentyone/TrenchTo3BallLine_GODSPEED2021pt2.java
@@ -8,21 +8,20 @@ import frc.team670.paths.Path;
 import frc.team670.robot.subsystems.DriveBase;
 
 /**
- * second part of godspeed trajectory
- * starts at trench then goes upder switch to intake 3 balls in a row
+ * fourth part of godspeed trajectory
+ * goes under switch to intake 3 balls in a row
  * fits 2021 field
  * google doc link: https://docs.google.com/document/d/1AsYxFidJCVz2cBFPJEAD1MU-b7dWLGTkcGLwushU1f8/edit?usp=sharing
  *
  * 
  * @author AkshatAdsule, EliseVbp
  */
-public class TrenchTo3BallLine_GODSPEED2021 extends Path{
+public class TrenchTo3BallLine_GODSPEED2021pt2 extends Path{
 
-        public TrenchTo3BallLine_GODSPEED2021(DriveBase driveBase) {
+        public TrenchTo3BallLine_GODSPEED2021pt2(DriveBase driveBase) {
                 super(
 
                         List.of(
-                                new Pose2d(7.8, -0.706, Rotation2d.fromDegrees(0)),
                                 new Pose2d(7.985, -2.804, Rotation2d.fromDegrees(-159.174)),
                                 new Pose2d(6.453, -3.441, Rotation2d.fromDegrees(-159.174))
                         ), 
