b9be3a18680df1acf7c54fe45977577ba356bf61
aaditya_raj <theultimatearaj@gmail.com>
2021-07-12 16:18:30 -0700

    integrated photonlib

--- 2020-Robot/src/main/java/frc/team670/robot/commands/vision/PhotonVision.java
+++ 2020-Robot/src/main/java/frc/team670/robot/commands/vision/PhotonVision.java
@@ -1,37 +1,48 @@
 package frc.team670.robot.commands.vision;
-import edu.wpi.first.wpilibj2.*;
+import edu.wpi.first.wpilibj.*;
 import frc.team670.robot.constants.RobotConstants;
 import frc.team670.robot.subsystems.DriveBase;
+import frc.team670.mustanglib.utils.MustangController;
 
-import org.photonvision.*;
 
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
 public class PhotonVision {
 
     PhotonCamera camera;
-    
+    MustangController mController;
+
     public PhotonVision(){
         camera = new PhotonCamera("photonvision");
+        mController = new MustangController(0);
     }
 
     public double[] getVisionValues() {
-        final double CAMERA_HEIGHT_METERS = Units.inchesToMeters(24);
-        final double TARGET_HEIGHT_METERS = Units.feetToMeters(5);
+        final double CAMERA_HEIGHT_METERS = 0.9144;
+        final double TARGET_HEIGHT_METERS = 32.9184;
+
+        double distance = 0.0, angle = 0.0;
 
         // Angle between horizontal and the camera.
-        final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(0);
-        if (xboxController.getAButton()) {
+        final double CAMERA_PITCH_RADIANS = 0;
+        if (mController.getAButton()) {
             // Vision-alignment mode
             // Query the latest result from PhotonVision
             var result = camera.getLatestResult();
             if (result.hasTargets()) {
                 // First calculate range
-                double range = PhotonUtils.calculateDistanceToTargetMeters(
+                distance = PhotonUtils.calculateDistanceToTargetMeters(
                             CAMERA_HEIGHT_METERS,
                             TARGET_HEIGHT_METERS,
                             CAMERA_PITCH_RADIANS,
-                            Units.degreesToRadians(result.getBestTarget().getPitch()));
+                            result.getBestTarget().getPitch() * Math.PI/180);
+                angle = result.getBestTarget().getYaw();
             }
         }
+        // System.out.println("Angle: " + angle + ", Distance: " + distance);
+        double[] visionValues = new double[] {angle, distance};
+        return visionValues;
+
 
     }
 }
