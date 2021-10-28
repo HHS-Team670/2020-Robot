865a05b7a6ba10f20351d4a4ac0f8403bb7fb558
Laksh Bhambhani <lakshbh@gmail.com>
2021-10-27 23:05:33 -0700

    Toggle shooter fixes

--- 2020-Robot/src/main/java/frc/team670/robot/commands/shooter/ToggleShooter.java
+++ 2020-Robot/src/main/java/frc/team670/robot/commands/shooter/ToggleShooter.java
@@ -5,35 +5,40 @@ import java.util.Map;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.team670.mustanglib.commands.MustangCommand;
-import frc.team670.robot.constants.FieldConstants;
-import frc.team670.robot.subsystems.DriveBase;
 import frc.team670.mustanglib.subsystems.MustangSubsystemBase;
-import frc.team670.robot.subsystems.Shooter;
 import frc.team670.mustanglib.subsystems.MustangSubsystemBase.HealthState;
+import frc.team670.mustanglib.utils.Logger;
 import frc.team670.mustanglib.utils.functions.MathUtils;
+import frc.team670.robot.subsystems.DriveBase;
+import frc.team670.robot.subsystems.Shooter;
+import frc.team670.robot.subsystems.Vision;
 
 public class ToggleShooter extends InstantCommand implements MustangCommand {
 
     private Shooter shooter;
-    private DriveBase driveBase;
+    private Vision vision;
 
-    public ToggleShooter(Shooter shooter, DriveBase driveBase) {
+    private double targetRPM;
+
+    public ToggleShooter(Shooter shooter, Vision vision) {
         this.shooter = shooter;
-        this.driveBase = driveBase;
+        this.vision = vision;
     }
 
     @Override
     public void initialize() {
         super.initialize();
         if (MathUtils.doublesEqual(0.0, shooter.getStage2Velocity(), 10)) {
-            // double currentX = driveBase.getPose().getTranslation().getX();
-            // double currentY = driveBase.getPose().getTranslation().getY();
-            // double distanceToTarget = Math.sqrt(
-            //     (Math.pow(currentX - FieldConstants.FIELD_ORIGIN_TO_OUTER_GOAL_CENTER_X_METERS, 2) +
-            //      Math.pow(currentY, 2))
-            // );
-            // double targetRPM = shooter.getTargetRPMForDistance(distanceToTarget);
-            // shooter.setVelocityTarget(targetRPM);
+            if(vision.getHealth(true) == HealthState.GREEN){
+                double distanceToTarget = vision.getDistanceToTargetM();
+                Logger.consoleLog("Shooter distance to target %s", distanceToTarget);
+                targetRPM = shooter.getTargetRPMForDistance(distanceToTarget);
+            }
+            else{
+                targetRPM = shooter.getDefaultRPM();
+            }
+            Logger.consoleLog("Shooter Stage 2 RPM should be %s", targetRPM);
+            shooter.setVelocityTarget(targetRPM);
             shooter.setRampRate(true);
             shooter.run();
         } else {
